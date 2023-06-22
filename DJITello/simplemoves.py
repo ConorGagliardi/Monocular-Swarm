b95799902e0cfa959199e2d55537e18ae11d0540
Conor Gagliardi <112734764+ConorGagliardi@users.noreply.github.com>
2023-06-22 10:41:32 -0500

    Update simplemoves.py

--- DJITello/simplemoves.py
+++ DJITello/simplemoves.py
@@ -1,17 +1,17 @@
 def move_up(drone, distance):
-    drone.move_up(distance * 15)
+    drone.move_up(distance * 25)
 
 def move_down(drone, distance):
-    drone.move_down(distance * 15)
+    drone.move_down(distance * 25)
 
 def move_forward(drone, distance):
-    drone.move_forward(distance * 15)
+    drone.move_forward(distance * 25)
 
 def move_backward(drone, distance):
-    drone.move_back(distance * 15)
+    drone.move_back(distance * 25)
 
 def move_right(drone, distance):
-    drone.move_right(distance * 15)
+    drone.move_right(distance * 25)
 
 def move_left(drone, distance):
-    drone.move_left(distance * 15)
+    drone.move_left(distance * 25)
